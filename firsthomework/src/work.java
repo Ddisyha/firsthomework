574399c68b5ccb2c664f6f1a237f0d6555ccaf82
Ddisyha <126403899+Ddisyha@users.noreply.github.com>
2025-03-25 18:50:34 +0500

    Add files via upload

---
+++ firsthomework/src/work.java
@@ -0,0 +1,15 @@
+public class work {
+    public static void main(String[] args) {
+        // Первое задание
+        System.out.println("Filipp Voronov");
+
+        // Второе задание
+        int income = 44000;
+        int spending = 30000;
+        int balance = income - spending;
+
+        System.out.println("Итого (руб)");
+        System.out.println(balance);
+    }
+
+}
